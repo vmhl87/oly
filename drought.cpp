ffafdb504d1795af63110ecbd633319b164e5eaa
Vincent Loh <vincentmloh87@gmail.com>
2023-08-30 16:23:11 -0400

    preliminary work on drought (2022 jan bronze 3)

---
+++ drought.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void test_case(){
+	int n_cows;
+	cin >> n_cows;
+	int cows[n_cows];
+	for(int i=0;i<n_cows;i++) cin >> cows[i];
+	
+}
+
+int main(){
+	test_case();
+	return 1;
+}
