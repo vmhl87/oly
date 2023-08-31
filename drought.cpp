649401e14c82c005fada3de0c4d477e3b9a0b613
Vincent Loh <vincentmloh87@gmail.com>
2023-08-31 15:02:11 -0400

    Worked on a basic semi-solution, seems to fail on test cases 9 and up This is inline with the restrictions (below 9, each h_i is limited to 100 - this may be the issue? deterministically it should not matter, maybe the issue lies in integer limit?

--- drought.cpp
+++ drought.cpp
@@ -4,12 +4,59 @@ using namespace std;
 void test_case(){
 	int n_cows;
 	cin >> n_cows;
-	int cows[n_cows];
+	long long cows[n_cows];
 	for(int i=0;i<n_cows;i++) cin >> cows[i];
-	
+	/*
+		define index as a bound such that all cows
+		  before such index are at the same hunger level
+		while index is not the last cow:
+			look at the first pair of cows that differ
+			in hunger level: if the first is smaller than
+			the second, this is trivial
+			if not, decrement all cows before index in a
+			deterministic pattern
+	*/
+	int count=0; //bags of corn used
+	for(int index=0;index<n_cows-1;index++){
+		if(cows[index]==cows[index+1]) continue;
+		if(cows[index]<cows[index+1]){
+			if(index<n_cows-2){
+				count+=cows[index+1]-cows[index];
+				cows[index+2]-=cows[index+1]-cows[index];
+				if(cows[index+2]<0){
+					cout << -1 << '\n';
+					return;
+				}
+				cows[index+1]=cows[index];
+			}else{
+				cout << -1 << '\n';
+				return;
+			}
+		}else{
+			if(index>0 && index%2>0){
+				//even = all can be decremented
+				//(index+1)/2 pairings
+				long long diff=cows[index]-cows[index+1];
+				count+=diff*(index+1)/2;
+				if(cows[0]<diff){
+					cout << -1 << '\n';
+					return;
+				}
+				for(int i=0;i<=index;i++) cows[i]-=diff;
+			}else{
+				cout << -1 << '\n';
+				return;
+			}
+		}
+	}
+	cout << count*2 << '\n';
 }
 
 int main(){
-	test_case();
+	int cases;
+	cin >> cases;
+	for(int i=0;i<cases;i++){
+		test_case();
+	}
 	return 1;
 }
