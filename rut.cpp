082c8dc0b0e0bc3df699cc4b1be756da86e97119
Vincent Loh <vincent_@videdit2.iidxa.com>
2023-09-05 18:35:09 -0400

    Solve Stuck In A Rut (2020 dec bronze 3)

---
+++ rut.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct cow{
+	int x;
+	int y;
+	int id;
+};
+
+struct event{
+	int time;
+	int bound;
+	int upper;
+	int lower;
+};
+
+bool comp_e_cows(struct cow i, struct cow j){
+	return (i.y<j.y);
+}
+
+bool comp_n_cows(struct cow i, struct cow j){
+	return (i.x<j.x);
+}
+
+bool comp_events(struct event i, struct event j){
+	return (i.time<j.time);
+}
+
+int main(){
+	int n;
+	cin>>n;
+	vector<struct cow> e_cows;
+	vector<struct cow> n_cows;
+	int id=0;
+	for(int i=0;i<n;i++){
+		char c;
+		int x,y;
+		cin>>c>>x>>y;
+		struct cow tmp;
+		tmp.x=x;tmp.y=y;tmp.id=id;
+		id++;
+		if(c=='E')e_cows.push_back(tmp);
+		else n_cows.push_back(tmp);
+	}
+	sort(e_cows.begin(),e_cows.end(),comp_e_cows);
+	sort(n_cows.begin(),n_cows.end(),comp_n_cows);
+	//for(struct cow c:e_cows)
+	//	cout<<c.id<<' '<<c.x<<' '<<c.y<<'\n';cout<<'\n';
+	//for(struct cow c:n_cows)
+	//	cout<<c.id<<' '<<c.x<<' '<<c.y<<'\n';cout<<'\n';
+	vector<struct event> events;
+	for(struct cow e_cow:e_cows)
+		for(struct cow n_cow:n_cows)
+			if(e_cow.x<n_cow.x&&e_cow.y>n_cow.y){
+				struct event tmp;
+				if(n_cow.x-e_cow.x<e_cow.y-n_cow.y){
+					tmp.time=e_cow.y-n_cow.y;
+					tmp.bound=n_cow.x-e_cow.x;
+					tmp.upper=e_cow.id;
+					tmp.lower=n_cow.id;
+				}else if(e_cow.y-n_cow.y<n_cow.x-e_cow.x){
+					tmp.time=n_cow.x-e_cow.x;
+					tmp.bound=e_cow.y-n_cow.y;
+					tmp.upper=n_cow.id;
+					tmp.lower=e_cow.id;
+				}else continue;
+				events.push_back(tmp);
+			}
+	sort(events.begin(),events.end(),comp_events);
+	//for(struct event e:events)
+	//	cout<<e.time<<' '<<e.upper<<' '<<e.lower<<'\n';
+	int ret[n];
+	for(int i=0;i<n;i++)ret[i]=-1;
+	for(struct event e:events){
+		if(ret[e.lower]>-1)continue;
+		if(ret[e.upper]==-1||ret[e.upper]>=e.bound)
+			ret[e.lower]=e.time;
+	}
+	for(int i=0;i<n;i++){
+		if(ret[i]==-1)cout<<"Infinity\n";
+		else cout<<ret[i]<<'\n';
+	}
+	return 1;
+}
