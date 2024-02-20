b90fe16cdfad59d4cb0d3f04b5b093e509c12f94
Vincent Loh <vincentmloh87@gmail.com>
2024-02-20 16:42:55 -0500

    solved counting_numbers

--- cses/counting_numbers.cpp
+++ cses/counting_numbers.cpp
@@ -1,73 +1,113 @@
 #include <iostream>
 using namespace std;
 
-#define MOD 1000000007
-
-int dp[19][10];
+long long dp[19][10];
 bool f[19][10];
 
 // # of sequences ending (biggest digit) in end with length n
 long long seq(int end,int n){
-//cout<<"  sequence ending "<<end<<" length "<<n<<'\n';
+//cout<<"   seq called on "<<end<<','<<n<<'\n';
 	if(n==1)return 1;
 	if(f[n][end])return dp[n][end];
 	long long ret=0;
 	for(int i=0;i<10;i++)
-		if(i!=end)ret=(ret+seq(i,n-1))%MOD;
+		if(i!=end)ret+=seq(i,n-1);
 	f[n][end]=1;dp[n][end]=ret;
 	return ret;
 }
 
-// bound: all numbers in [0, end*10^zr)
-long long sp(int end,int zr,int bef){
-cout<<" range ending "<<end<<" before "<<bef<<" zeroes "<<zr<<'\n';
-	if(!end)return 0;
-	if(!zr){
+// n: number to fracture, z: hidden zeroes
+long long fract(long long n,int z){
+//cout<<"  recieved "<<n<<','<<z<<'\n';
+	// edge case: n < 10
+	if(n<10){
+		// normally we don't count n
 		long long ret=0;
-		for(int i=0;i<end;i++)
-			if(i!=bef)ret++;
+		// we only count n when z=0, no preceding
+		if(!z)ret=1;
+		//  in our example, this would be 0000 - 2999 so
+		//	we have to divide into sections: first
+		//		2000 - 2999
+		//		1000 - 1999
+		//	then
+		//		100 - 999
+		//		10 - 99
+		//		1 - 9
+		// clip down n
+		n=n%10;
+		// stage 1
+		for(int i=1;i<n;i++)
+			//  everything from i000 to i999
+			//	starts i, zeroes z -> length z+1
+			ret+=seq(i,z+1);
+		// stage 2
+		for(int i=0;i<z;i++)
+			// essentially a sequence of length i+1 starting in 0
+			ret+=seq(0,i+2);
+//cout<<"  return "<<ret<<'\n';
 		return ret;
 	}
-	// starts wih the null case (0)
-	long long ret=1;
-	// collects all starting 1-end-1
-	for(int i=1;i<end;i++)
-		// starts i, length zr
-		for(int j=0;j<10;j++)
-			if(j!=end&&j!=bef)ret=(ret+seq(j,zr))%MOD;
-	// collects all between 1 to 9..zr..9
-	for(int i=1;i<=zr;i++)
-		// starts with 1-9
-		for(int j=1;j<10;j++)
-			ret=(ret+seq(j,i))%MOD;
-cout<<" :"<<ret<<'\n';
-	return ret;
-}
-
-long long all(int n){
-	long long ret=0;
-	int zr=0;
-	int rev[19];
-	while(n){
-		rev[zr]=n%10;
-		n/=10;zr++;
+	// split units digit from n
+	int u=n%10;n/=10;
+	// check for adjacent identical digits
+	long long t=n;
+	int last=-1;
+	while(t){
+		// if there is overlap, just return 0
+//if(t%10==last)cout<<"  overlap\n";
+		if(t%10==last)return 0;
+		last=t%10;
+		t/=10;
 	}
-	for(int i=0;i<zr-1;i++)
-		ret=(ret+sp(rev[i],i,rev[i+1]))%MOD;
-	ret=(ret+sp(rev[zr-1],zr-1,-1))%MOD;
+	// we can assume no adj - discard rest of n
+	n=n%10;
+	// we don't count n unless special case
+	long long ret=0;
+	// z=0, n!=u
+	if(!z&&n!=u)ret=1;
+	//  in example: from 3125: the range 3100-3120 represented as
+	//	312; we slice this down to n=1 u=2, so we iterate through all
+	//	numbers starting 0 to u-1 ending in z zeroes - this would be
+	//	from 3100 to 3119 (doesn't count n)
+	for(int i=0;i<u;i++)if(i!=n)
+		//  sequence in question has z zeroes so therefore z+1 length
+		//	and starts with i (when z = 0, this is consistent)
+		ret+=seq(i,z+1);
+//cout<<"  return "<<ret<<'\n';
 	return ret;
 }
 
-long long bforce(int n){
+// find all from 0 to n, inclusive
+long long all(long long n){
+	// weird edge case
+	if(!n)return 1;
+//cout<<"process "<<n<<'\n';
+	// account for 0
+	long long ret=1;
+	int z=0;
+	int pow=1;
+	while(n){
+		// process last digit
+//cout<<" fracture "<<(n-n%10)*pow<<" - "<<n*pow<<'\n';
+		ret+=fract(n,z);
+		n/=10;z++;pow*=10;
+	}
+	return ret;
+}
+
+// simple brute force
+long long bforce(long long n){
 	long long ret=0;
-	for(int i=0;i<n;i++){
-		int last=-1,s=i;
+	for(long long i=0;i<=n;i++){
+		int last=-1;
+		long long s=i;
 		bool w=1;
 		while(s){
 			if(s%10==last)w=0;
 			last=s%10;
 			s/=10;
 		}
+//cout<<i<<" does ";if(!w)cout<<"not ";cout<<"work\n";
 		if(w)ret++;
 	}
 	return ret;
@@ -77,11 +117,11 @@ int main(){
 	for(int i=0;i<19;i++)
 		for(int j=0;j<10;j++)
 			f[i][j]=0;
-	int a,b;cin>>a>>b;
-	long long ret=all(b+1)+MOD-all(a);
-	ret%=MOD;
+	long long a,b;cin>>a>>b;
+//cout<<all(a)<<'\n'<<all(b)<<'\n';
+	long long ret=all(b);
+	if(a>0)ret-=all(a-1);
 	cout<<ret<<'\n';
-	ret=bforce(b+1)+MOD-bforce(a);
-	ret%=MOD;
-	cout<<ret<<'\n';
+//	ret=bforce(b)-bforce(a-1);
+//	cout<<ret<<'\n';
 }
--- cses/counting_numbers_old.cpp
+++ cses/counting_numbers_old.cpp
@@ -6,44 +6,61 @@ using namespace std;
 int dp[19][10];
 bool f[19][10];
 
-long long seq(int n,int end){
-	cout<<"running seq on "<<n<<' '<<end<<'\n';
+// # of sequences ending (biggest digit) in end with length n
+long long seq(int end,int n){
+//cout<<"  sequence ending "<<end<<" length "<<n<<'\n';
 	if(n==1)return 1;
 	if(f[n][end])return dp[n][end];
 	long long ret=0;
 	for(int i=0;i<10;i++)
-		if(i!=end)ret=(ret+seq(n-1,i))%MOD;
+		if(i!=end)ret=(ret+seq(i,n-1))%MOD;
 	f[n][end]=1;dp[n][end]=ret;
 	return ret;
 }
 
-long long sp(int first,int z){
-	if(!z)return first;
+// bound: all numbers in [0, end*10^zr)
+long long sp(int end,int zr,int bef){
+cout<<" range ending "<<end<<" before "<<bef<<" zeroes "<<zr<<'\n';
+	if(!end)return 0;
+	if(!zr){
+		long long ret=0;
+		for(int i=0;i<end;i++)
+			if(i!=bef)ret++;
+		return ret;
+	}
+	// starts wih the null case (0)
 	long long ret=1;
-	for(int i=1;i<first;i++)
+	// collects all starting 1-end-1
+	for(int i=1;i<end;i++)
+		// starts i, length zr
 		for(int j=0;j<10;j++)
-			if(j!=i)ret=(ret+seq(z,j))%MOD;
-	for(int i=1;i<=z;i++)
+			if(j!=end&&j!=bef)ret=(ret+seq(j,zr))%MOD;
+	// collects all between 1 to 9..zr..9
+	for(int i=1;i<=zr;i++)
+		// starts with 1-9
 		for(int j=1;j<10;j++)
-			ret=(ret+seq(i,j))%MOD;
+			ret=(ret+seq(j,i))%MOD;
+cout<<" :"<<ret<<'\n';
 	return ret;
 }
 
 long long all(int n){
-cout<<"all on "<<n<<'\n';
-	int z=0;
 	long long ret=0;
+	int zr=0;
+	int rev[19];
 	while(n){
-cout<<"	segment "<<n%10<<','<<z<<'\n';
-		if(n%10>0)ret=(ret+sp(n%10,z))%MOD;
-		n/=10;z++;
+		rev[zr]=n%10;
+		n/=10;zr++;
 	}
+	for(int i=0;i<zr-1;i++)
+		ret=(ret+sp(rev[i],i,rev[i+1]))%MOD;
+	ret=(ret+sp(rev[zr-1],zr-1,-1))%MOD;
 	return ret;
 }
 
 long long bforce(int n){
 	long long ret=0;
-	for(int i=1;i<n;i++){
+	for(int i=0;i<n;i++){
 		int last=-1,s=i;
 		bool w=1;
 		while(s){
