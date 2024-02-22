c86f91d7ca61a265190b9b005f54cdf9a4fdc271
Vincent Loh <vincentmloh87@gmail.com>
2024-02-22 17:29:10 -0500

    solved sum of 4 with a greedy algorithm, realized not everything is DP :p

--- cses/sum_of_4.cpp
+++ cses/sum_of_4.cpp
@@ -1,39 +1,62 @@
+/*  Sum of Four Values
+
+	We bruteforce the first two values, and use a
+	cool greedy algorithm to determine the last 2:
+	
+	Keep two pointers, one at the bottom of the search
+	range and another at the top, and update them as
+	accordingly:
+		if the sum is too big, decrement top pointer
+		if sum is too small, increment bottom pointer
+		otherwise, we are finished
+	
+	If we bounds check so that none of these pointers
+	ever overlap, this is actually fast enough to pass
+*/
+
+#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <map>
 using namespace std;
 
-typedef struct{
-	int n[4];
-	int c=0;
+// datastructure to hold values,indexes and sort
+typedef struct val{
+	long long v;
+	int i;
+	bool operator<(const val o)const{
+		return v<o.v;
+	}
+	bool operator==(const val o)const{
+		return v==o.v;
+	}
 }val;
 
 int main(){
-	int n;long long x;cin>>n>>x;
-	val *dp=new val[x*4+4];
-	for(int r=0;r<n;r++){
-		long long t;cin>>t;
-		for(long long i=3;i>0;i--){
-			for(long long j=0;j<=x-t;j++){
-				if(dp[j*4+i-1].c&&!dp[j*4+t*4+i].c){
-					dp[j*4+t*4+i].c=i+1;
-					for(int k=0;k<i;k++){
-						dp[j*4+t*4+i].n[k]=dp[j*4+i-1].n[k];
-					}
-					dp[j*4+t*4+i].n[i]=r;
-				}
+	int n;long long x;
+	cin>>n>>x;
+	val ns[n];
+	for(int i=0;i<n;i++){
+		cin>>ns[i].v;
+		ns[i].i=i+1;
+	}
+	sort(ns,ns+n);
+	// bruteforce first 2 (i,j)
+	for(int i=0;i<n-3,ns[i].v<=x/4;++i){
+		long long s=x-ns[i].v;
+		for(int j=i+1;j<n-2,ns[j].v<=s/3;++j){
+			// ts = target sum
+			long long ts=s-ns[j].v;
+			// l,u = lower,upper
+			int l=j+1,u=n-1;
+			// iterate until works
+			while(l<u&&ns[l].v+ns[u].v!=ts){
+				if(ns[l].v+ns[u].v<ts)l++;
+				else u--;
+			}
+			if(l<u){
+				cout<<ns[i].i<<' '<<ns[j].i<<' '<<ns[l].i<<' '<<ns[u].i<<'\n';
+				return 0;
 			}
 		}
-		if(t<=x&&!dp[t*4].c){
-			dp[t*4].n[0]=r;
-			dp[t*4].c=1;
-		}
 	}
-	if(dp[x*4+3].c){
-		for(int i=0;i<4;i++){
-			if(i)cout<<' ';
-			cout<<dp[x*4+3].n[i]+1;
-		}
-		cout<<'\n';
-	}else cout<<"IMPOSSIBLE\n";
+	cout<<"IMPOSSIBLE\n";
 }
