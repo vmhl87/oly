91c90a895efcd9b39236e060c16ed04b41987beb
Vincent Loh <vincentmloh87@gmail.com>
2024-02-24 20:27:17 -0500

    almost finished

--- codeforces/slimes.cpp
+++ codeforces/slimes.cpp
@@ -7,41 +7,53 @@ void test_case(){
 	int s[n];for(int i=0;i<n;++i)cin>>s[i];
 	int p[n+2];p[0]=-s[0];p[1]=0;
 	for(int i=0;i<n;++i)p[i+2]=p[i+1]+s[i];
+	
 	int lm[n],um[n];
-	lm[0]=1;int last=s[0],bl=1;
-	bool lag=0;
+	int last=s[0],ln=0;lm[0]=0;
 	for(int i=1;i<n;++i){
-		if(s[i]==last||lag)bl++;
-		else bl=1;
-		lm[i]=bl;
-		lag=0;if(s[i]==last)lag=1;
+		if(last==s[i]||(ln&&last<s[i]))ln++;
+		else ln=0;
+		lm[i]=ln;
+		if(last!=s[i])ln=0;
 		last=s[i];
 	}
-	lag=0;bl=1;um[n-1]=1;
-	last=s[n-1];
+	last=s[n-1];ln=0;um[n-1]=0;
 	for(int i=n-2;i>=0;--i){
-		if(s[i]==last||lag)bl++;
-		else bl=1;
-		um[i]=bl;
-		lag=0;if(s[i]==last)lag=1;
+		if(last==s[i]||(ln&&last<s[i]))ln++;
+		else ln=0;
+		um[i]=ln;
+		if(last!=s[i])ln=0;
 		last=s[i];
 	}
+	
 for(int i:lm)cout<<i<<' ';cout<<'\n';
 for(int i:um)cout<<i<<' ';cout<<'\n';
+	
 	for(int i=0;i<n;++i){
-//		if(i)cout<<' ';
-		auto l=lower_bound(p,p+n+2,p[i+1]-s[i]-1),
+		if(i)cout<<' ';
+		auto l=lower_bound(p,p+n+2,p[i+1]-s[i])-1,
 			u=upper_bound(p,p+n+2,p[i+2]+s[i]);
 		int lb=n+n,ub=n+n;
 		if(l!=p)lb=p+i+1-l;
-		if(u!=p+n+1)ub=u-p-i-2;
-		if(lb<lm[i]&&(lb>1&&s[i]!=s[i-1]))lb=lm[i];
-		if(ub<um[i]&&(ub>1&&s[i]!=s[i+1]))ub=um[i];
+		if(u!=p+n+2)ub=u-p-i-2;
+		
+//cout<<':'<<i<<' '<<s[i]<<'\n';
+//cout<<"  searching for "<<p[i+1]-s[i]-1<<','<<p[i+2]+s[i]<<'\n';
+//cout<<"  found at "<<*l<<','<<*u<<'\n';
+//cout<<" :"<<l-p<<' '<<u-p<<'\n';
+//cout<<"  "<<i+1<<' '<<i+2<<'\n';
+		
+		if(lb<=lm[i])lb=lm[i]+1;
+		if(ub<=um[i])ub=um[i]+1;
+
+//cout<<" result: ";
+		
 		int best=min(lb,ub);
-cout<<lb<<' '<<ub<<' '<<l-p<<' '<<u-p<<' '<<p[i+1]-s[i]-1<<' '<<p[i+2]+s[i]<<"\n :";
 		if(best==n+n)cout<<-1;
 		else cout<<best;
-cout<<'\n';
+
+//cout<<'\n';
+
 	}
 	cout<<'\n';
 }
