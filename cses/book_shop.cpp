3ecd2cdb81221c3ae283108044996aa46e3ac27b
Vincent Loh <vincentmloh87@gmail.com>
2024-02-27 20:54:27 -0500

    another attempt at book shop, will debug fenwick

--- cses/book_shop.cpp
+++ cses/book_shop.cpp
@@ -2,26 +2,26 @@
 #include <iostream>
 using namespace std;
 
-using book=struct book;
-struct book{
+typedef struct book{
 	int price;
-	int pages;
+	long long pages;
 	bool operator<(const book o)const{
+		if(price==o.price)return pages<o.pages;
 		return price<o.price;
 	}
-};
+}book;
 
 int main(){
 	int n,x;cin>>n>>x;
-	int best[x+1];
+	long long best[x+1];
 	for(int i=0;i<x;i++)best[i+1]=-1;
 	best[0]=0;
 	book books[n];
-	for(int i=0;i<n;i++)cin>>books[i].price;
-	for(int i=0;i<n;i++)cin>>books[i].pages;
+	for(int i=0;i<n;++i)cin>>books[i].price;
+	for(int i=0;i<n;++i)cin>>books[i].pages;
 	sort(books,books+n);
-	for(int i=0;i<n;i++)
-		for(int j=x-books[i].price;j>=0;j--)if(best[j]>-1){
+	for(int i=0;i<n;++i)
+		for(int j=x-books[i].price;j>=0;--j)if(best[j]>-1){
 			int k=j+books[i].price;
 			if(best[k]<best[j]+books[i].pages)
 				best[k]=best[j]+books[i].pages;
--- review/fenwicktree.cpp
+++ review/fenwicktree.cpp
@@ -5,32 +5,32 @@ using namespace std;
 typedef struct fenwicktree{
 	vector<int> tree;
 	int n;
-    int sum(int i){
-        int ret=0;
-        while(i>0){
-            ret+=tree[i-1];
-            i=(i&(i+1))-1;
-        }
-        return ret;
-    }
-    fenwicktree(vector<int> v):tree(v.size()),n(v.size()){
-        for(int i=0;i<n;++i)tree[i]=v[i]+sum(i);
-    }
-    int range(int l,int r){
-        return sum(r)-sum(l-1);
-    }
+	int sum(int i){
+		int ret=0;
+		while(i>0){
+			ret+=tree[i-1];
+			i=(i&(i+1))-1;
+		}
+		return ret;
+	}
+	fenwicktree(vector<int> v):tree(v.size()),n(v.size()){
+		for(int i=0;i<n;++i)tree[i]=v[i]+sum(i);
+	}
+	int range(int l,int r){
+		return sum(r)-sum(l-1);
+	}
 }fenwicktree;
 
 int main(){
-    int n;cin>>n;
-    vector<int> v(n);
-    for(int i=0;i<n;++i)
-        cin>>v[i];
-    fenwicktree m(v);
-    for(;;){
-        int t;cin>>t;
-        if(!t)break;
-        int b;cin>>b;
-        cout<<m.range(t,b)<<'\n';
-    }
+	int n;cin>>n;
+	vector<int> v(n);
+	for(int i=0;i<n;++i)
+		cin>>v[i];
+	fenwicktree m(v);
+	for(;;){
+		int t;cin>>t;
+		if(!t)break;
+		int b;cin>>b;
+		cout<<m.range(t,b)<<'\n';
+	}
 }
