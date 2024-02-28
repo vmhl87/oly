075aa4bf1445e6154c9770f9f32b7f00d89267b9
Vincent Loh <vincentmloh87@gmail.com>
2024-02-27 20:27:12 -0500

    working on fenwick tree imp

--- review/fenwicktree.cpp
+++ review/fenwicktree.cpp
@@ -1,33 +1,36 @@
-struct FenwickTree {
-    vector<int> bit;
-    int n;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-    FenwickTree(int n) {
-        this->n = n;
-        // fill tree with n values of 0
-        bit.assign(n, 0);
-    }
-
-	// constructor passing int
-    FenwickTree(vector<int> const &a) : FenwickTree(a.size()) {
-        for (size_t i = 0; i < a.size(); i++)
-            add(i, a[i]);
-    }
-
-    int sum(int r) {
-        int ret = 0;
-        for (; r >= 0; r = (r & (r + 1)) - 1)
-            ret += bit[r];
+typedef struct fenwicktree{
+	vector<int> tree;
+	int n;
+    int sum(int i){
+        int ret=0;
+        while(i>0){
+            ret+=tree[i-1];
+            i=(i&(i+1))-1;
+        }
         return ret;
     }
-
-    int sum(int l, int r) {
-        return sum(r) - sum(l - 1);
+    fenwicktree(vector<int> v):tree(v.size()),n(v.size()){
+        for(int i=0;i<n;++i)tree[i]=v[i]+sum(i);
     }
-
-    void add(int idx, int delta) {
-        for (; idx < n; idx = idx | (idx + 1))
-            bit[idx] += delta;
+    int range(int l,int r){
+        return sum(r)-sum(l-1);
     }
-};
+}fenwicktree;
 
+int main(){
+    int n;cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;++i)
+        cin>>v[i];
+    fenwicktree m(v);
+    for(;;){
+        int t;cin>>t;
+        if(!t)break;
+        int b;cin>>b;
+        cout<<m.range(t,b)<<'\n';
+    }
+}
