6defa7e5c83b92b9f723d381840b31d73e07cbc1
Vincent Loh <vincentmloh87@gmail.com>
2024-02-28 19:00:14 -0500

    finish initial barn tree sol

--- usaco/barn_tree.cpp
+++ usaco/barn_tree.cpp
@@ -33,8 +33,9 @@ int main(){
 		barns[b].adj.push(a);
 	}
 	// save operations
-	vector<op> ops;
-	ops.reserve(n-1);
+	int ops=0;
+	stack<op> dep[n];
+	stack<int> root;
 	// root the tree arbitrarily - at 0?
 	bool vis[n];for(int i=0;i<n;++i)vis[i]=1;
 	stack<int> dfs;dfs.push(0);vis[0]=0;
@@ -44,14 +45,20 @@ int main(){
 			// relax along edge and proc
 			dfs.pop();
 			if(!dfs.empty()){
-cout<<"relaxing node "<<t<<" to "<<dfs.top()<<'\n';
+// cout<<"relaxing node "<<t<<" to "<<dfs.top()<<'\n';
 				// compare under to w*avg
 				op nx;nx.a=t;nx.b=dfs.top();
 				nx.m=barns[t].under-barns[t].w*avg;
 				if(nx.m<0){
 					nx.m=-nx.m;nx.a=dfs.top();nx.b=t;
 				}
-				if(nx.m!=0)ops.push_back(nx);
+				if(nx.m>0){
+					ops++;
+					// depends barn t have >= m hay
+					dep[t].push(nx);
+					if(barns[t].h>=nx.m)
+						root.push(t);
+				}
 				// add weight + hay of previous
 				// node onto current one
 				int x=dfs.top();
@@ -61,12 +68,42 @@ cout<<"relaxing node "<<t<<" to "<<dfs.top()<<'\n';
 		}else{
 			int x=barns[t].adj.top();
 			if(vis[x]){
-cout<<"appending node "<<x<<" from "<<t<<'\n';
+// cout<<"appending node "<<x<<" from "<<t<<'\n';
 				vis[x]=0;
 				dfs.push(x);
 			}
 			barns[t].adj.pop();
 		}
 	}
-	cout<<ops.size()<<'\n';
+	cout<<ops<<'\n';
+	for(int i=0;i<n;++i)vis[i]=1;
+	while(!root.empty()){
+		int r=root.top();root.pop();
+		if(!vis[r])continue;
+		vis[r]=0;dfs.push(r);
+		while(!dfs.empty()){
+			int t=dfs.top();
+			if(dep[t].empty()){
+				dfs.pop();
+			}else{
+				op nx=dep[t].top();
+				dep[t].pop();
+				// print edge
+				cout<<nx.a+1<<' '<<nx.b+1
+					<<' '<<nx.m<<'\n';
+				if(vis[nx.b]){
+					vis[nx.b]=0;
+					dfs.push(nx.b);
+				}
+			}
+		}
+	}
+	for(int i=0;i<n;++i)if(!vis[i]){
+		while(!dep[i].empty()){
+			op x=dep[i].top();
+			cout<<x.a+1<<' '<<x.b+1
+				<<' '<<x.m<<'\n';
+			dep[i].pop();
+		}
+	}
 }
