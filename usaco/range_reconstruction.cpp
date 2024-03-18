7d418f3a4f76d4a98c81d6e75af30001f1c3502f
Vincent Loh <vincentmloh87@gmail.com>
2024-03-18 09:33:38 -0400

    fixed up range reconstruction

--- usaco/range_reconstruction.cpp
+++ usaco/range_reconstruction.cpp
@@ -9,25 +9,28 @@ int main(){
 		for(int j=i;j<n-1;++j)
 			cin>>r[i][j];
 	}
-// for(int i=0;i<n-1;++i){
-// 	for(int j:r[i])
-// 		cout<<j<<' ';
-// 	cout<<'\n';
-// }
 	int t;cin>>t;
+	// previous value
 	int last=0;
-	cout<<0<<' ';
+	cout<<1;
+	// ranges
 	pair<int,int> s[n];
+	// loop thru rest of values
 	for(int i=0;i<n-1;++i){
+		// upper and lower possible values
 		int up=last+r[i][i],
 			down=last-r[i][i];
+		// up and down work (?)
 		bool upw=1,downw=1;
+		// loop thru other constraints
 		for(int j=i-1;j>=0;--j){
 			// compare with s[j]
-			int diff=r[j][j]-s[j].second+s[j].first;
+			int diff=r[j][i]-s[j].second+s[j].first;
+			// within range
 			if(diff<1){
 				if(up>s[j].second)upw=0;
 				if(down<s[j].first)downw=0;
+			// out of range
 			}else{
 				if(up!=s[j].second+diff)upw=0;
 				if(down!=s[j].first-diff)downw=0;
@@ -36,7 +39,7 @@ int main(){
 		int next=down;
 		if(upw)next=up;
 		last=next;
-		cout<<next<<' ';
+		cout<<' '<<next+1;
 		// update ranges
 		s[i+1]=make_pair(next,next);
 		for(int j=0;j<=i;++j){
