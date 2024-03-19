e801147403c1d9a42c9ab6a7e12b9fdf9096af9b
Vincent Loh <vincentmloh87@gmail.com>
2024-03-19 11:48:54 -0400

    potentially fixed p1 sol

--- usaco/bessie_interview.cpp
+++ usaco/bessie_interview.cpp
@@ -1,71 +1,51 @@
 #include <iostream>
 #include <queue>
+#include <set>
+#include <map>
 using namespace std;
-
-vector<int> root;
-
-int findroot(int i){
-	queue<int> touch;
-	while(root[i]>-1){
-		touch.push(i);
-		i=root[i];
-	}
-	while(!touch.empty()){
-		root[touch.front()]=i;
-		touch.pop();
-	}
-	return i;
-}
-
-void unify(int a,int b){
-	queue<int> touch;
-	while(root[a]>-1){
-		touch.push(a);
-		a=root[a];
-	}
-	while(root[b]>-1){
-		touch.push(b);
-		b=root[b];
-	}
-	if(b!=a)root[b]=a;
-	while(!touch.empty()){
-		root[touch.front()]=a;
-		touch.pop();
-	}
-}
 
 int main(){
 	int n,k;cin>>n>>k;
-	root.resize(k,-1);
 	priority_queue<
 		pair<long long,pair<int,int>>,
 		vector<pair<long long,pair<int,int>>>,
 		greater<pair<long long,pair<int,int>>>
 	> farmers;
+	map<int,long long> id;
+	map<long long,set<int>> back;
 	for(int i=0;i<k;++i,--n){
 		long long j;cin>>j;
+		id[i]=0;back[j].insert(i);
 		farmers.push(make_pair(j,make_pair(i,1)));
 	}
+	int idx=k;
 	while(n--){
 		long long t,top=farmers.top().first;cin>>t;
-		queue<int> group;
-		int count=0,cur=farmers.top().second.first;
-		while(!farmers.empty()&&farmers.top().first==top){
+		int count=0,cur=farmers.top().second.first;queue<int> group;
+		while(farmers.size()&&farmers.top().first==top){
 			group.push(farmers.top().second.first);
 			count+=farmers.top().second.second;
 			farmers.pop();
 		}
-		farmers.push(make_pair(top+t,make_pair(cur,1)));
+		id[idx]=top;farmers.push(make_pair(top+t,make_pair(idx++,1)));
 		if(count>1)farmers.push(make_pair(top,make_pair(cur,count-1)));
-		while(!group.empty()){
-			unify(cur,group.front());
-			group.pop();
+		while(group.size()){
+			back[top+t].insert(group.front());group.pop();
 		}
 	}
-//cout<<"unions:\n";for(int i=0;i<k;++i)cout<<i<<':'<<findroot(i)<<'\n';
-	cout<<farmers.top().first<<'\n';
-	int end=findroot(farmers.top().second.first);
+	long long t=farmers.top().first;cout<<t<<'\n';
+	set<long long> vis;set<int> end;queue<long long> bfs;bfs.push(t);
+	while(bfs.size()){
+		int top=bfs.front();bfs.pop();
+		for(int x:back[top])
+			if(!end.count(x)){
+				end.insert(x);
+				if(!vis.count(id[x])){
+					vis.insert(id[x]);bfs.push(id[x]);
+				}
+			}
+	}
 	for(int i=0;i<k;++i)
-		cout<<(end==findroot(i));
+		cout<<end.count(i);
 	cout<<'\n';
 }
--- usaco/bessie_interview_old.cpp
+++ usaco/bessie_interview_old.cpp
@@ -1,4 +1,3 @@
-#include <unordered_set>
 #include <iostream>
 #include <queue>
 using namespace std;
@@ -7,12 +6,12 @@ vector<int> root;
 
 int findroot(int i){
 	queue<int> touch;
-	while(root[i]>0){
+	while(root[i]>-1){
 		touch.push(i);
-		i=root[i]-1;
+		i=root[i];
 	}
 	while(!touch.empty()){
-		root[touch.front()]=i+1;
+		root[touch.front()]=i;
 		touch.pop();
 	}
 	return i;
@@ -20,62 +19,53 @@ int findroot(int i){
 
 void unify(int a,int b){
 	queue<int> touch;
-	while(root[a]>0){
+	while(root[a]>-1){
 		touch.push(a);
-		a=root[a]-1;
+		a=root[a];
 	}
-	while(root[b]>0){
+	while(root[b]>-1){
 		touch.push(b);
-		b=root[b]-1;
+		b=root[b];
 	}
-	if(b!=a)root[b]=a+1;
+	if(b!=a)root[b]=a;
 	while(!touch.empty()){
-		root[touch.front()]=a+1;
+		root[touch.front()]=a;
 		touch.pop();
 	}
 }
 
 int main(){
 	int n,k;cin>>n>>k;
-	root.resize(k);
+	root.resize(k,-1);
 	priority_queue<
 		pair<long long,pair<int,int>>,
 		vector<pair<long long,pair<int,int>>>,
 		greater<pair<long long,pair<int,int>>>
 	> farmers;
-	for(int i=0;i<k;++i,n--){
-		long long t;cin>>t;
-		farmers.push(make_pair(t,make_pair(i,1)));
+	for(int i=0;i<k;++i,--n){
+		long long j;cin>>j;
+		farmers.push(make_pair(j,make_pair(i,1)));
 	}
 	while(n--){
 		long long t,top=farmers.top().first;cin>>t;
 		queue<int> group;
-		int count=0,cur=-1;
+		int count=0,cur=farmers.top().second.first;
 		while(!farmers.empty()&&farmers.top().first==top){
-			if(cur<0)cur=farmers.top().second.first;
-			if(count)group.push(farmers.top().second.first);
+			group.push(farmers.top().second.first);
 			count+=farmers.top().second.second;
 			farmers.pop();
 		}
-		farmers.push(make_pair(top+t,make_pair(cur,1)));count--;
-		if(count==0)continue;
-		farmers.push(make_pair(top,make_pair(cur,count)));
+		farmers.push(make_pair(top+t,make_pair(cur,1)));
+		if(count>1)farmers.push(make_pair(top,make_pair(cur,count-1)));
 		while(!group.empty()){
-//cout<<"unify "<<cur<<' '<<group.front()<<'\n';
 			unify(cur,group.front());
 			group.pop();
 		}
-//cout<<"time "<<top+t<<'\n';
 	}
 //cout<<"unions:\n";for(int i=0;i<k;++i)cout<<i<<':'<<findroot(i)<<'\n';
-	long long t=farmers.top().first;cout<<t<<'\n';
-	unordered_set<int> end;
-//if(t==8){cout<<"110\n";return 0;}
-	while(!farmers.empty()&&farmers.top().first==t){
-		end.insert(farmers.top().second.first);
-		farmers.pop();break;
-	}
+	cout<<farmers.top().first<<'\n';
+	int end=findroot(farmers.top().second.first);
 	for(int i=0;i<k;++i)
-		cout<<end.count(findroot(i));
+		cout<<(end==findroot(i));
 	cout<<'\n';
 }
