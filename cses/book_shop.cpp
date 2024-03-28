1aa42b53d130d83ef263fd7808ae5fafb1c4a152
Vincent Loh <vincentmloh87@gmail.com>
2024-03-27 20:45:52 -0400

    fixed book shop? odd logical error

--- cses/book_shop.cpp
+++ cses/book_shop.cpp
@@ -1,31 +1,28 @@
-#include <algorithm>
 #include <iostream>
 using namespace std;
 
 typedef struct book{
 	int price;
 	long long pages;
-	bool operator<(const book o)const{
-		if(price==o.price)return pages<o.pages;
-		return price<o.price;
-	}
 }book;
 
 int main(){
 	int n,x;cin>>n>>x;
+	// best # of pages that can be bought with cost x
 	long long best[x+1];
-	for(int i=0;i<x;i++)best[i+1]=-1;
-	best[0]=0;
+	// initialize
+	for(int i=0;i<=x;i++)best[i]=0;
 	book books[n];
 	for(int i=0;i<n;++i)cin>>books[i].price;
 	for(int i=0;i<n;++i)cin>>books[i].pages;
-	sort(books,books+n);
+	// for every book:
 	for(int i=0;i<n;++i)
-		for(int j=x-books[i].price;j>=0;--j)if(best[j]>-1){
+		// for all possible places to add book, add it
+		for(int j=x-books[i].price;j>=0;--j){
+			// new price of this block of pages
 			int k=j+books[i].price;
 			if(best[k]<best[j]+books[i].pages)
 				best[k]=best[j]+books[i].pages;
 		}
-	while(best[x]<0)x--;
 	cout<<best[x]<<'\n';
 }
