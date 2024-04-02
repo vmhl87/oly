0670935e284a342f07f189af7af2730353b5c924
Vincent Loh <vincentmloh87@gmail.com>
2024-04-02 09:27:55 -0400

    read editorial, attempting fix solution

--- contests/codeforces-mar30-24/farm_game.cpp
+++ contests/codeforces-mar30-24/farm_game.cpp
@@ -28,11 +28,27 @@ long long nck(long long n,long long k){
 
 void test_case(){
 	long long l,n;cin>>l>>n;
-	long long ret=2-(l&1);
-	ret=(ret*nck(n+1+((l-n-n-n)>>1),n+1))%M;
-//	cout<<"total:"<<ret<<'\n';
-//	cout<<ret<<':'<<n+1+((l-n-n-n)>>1)<<','<<n+1<<'\n';
-	ret=(ret+ret)%M;
+	// ret = all ways
+	long long ret=nck(l,n<<1);
+	// lose = ways where distances are even
+	// need to count for all amounts of odd seg
+	// between b_i, a_i+1
+	long long lose=0;
+	// i odd seg
+	for(long long i=0;i<n;++i){
+		// check if reachable
+		if(i+(n<<1)>l)break;
+		long long seg=1+((l^i)&1);
+		// n-1 locations, i sel
+		seg=(seg*nck(n-1,i))%M;
+		// (l-n-n-i)/2 blocks, n+n bars
+		seg=(seg*nck((n<<1)+((l-i-(n<<1))>>1),n<<1))%M;
+		lose+=seg;if(lose>=M)lose-=M;
+	}
+	// subtract
+	ret-=lose;if(ret<0)ret+=M;
+	// double
+	ret<<=1;if(ret>=M)ret-=M;
 	cout<<ret<<'\n';
 }
 
