6e71c5b701c8f39fc65f6f6bae4d75c6c30ec897
Vincent Loh <vincentmloh87@gmail.com>
2024-05-10 07:42:01 -0400

    solved this ez one (i was stupid before)

--- cses/reading_books.cpp
+++ cses/reading_books.cpp
@@ -1,19 +1,22 @@
+// Reading Books  -  https://cses.fi/problemset/task/1631
+// The key realization here is that a greedy algorithm exists:
+// Alice reads books 1 - n in order, while Bob reads boox n - 1
+// in order, where the books are sorted in increasing order of
+// length. There will be overlap if and only if at some point
+// Alice and Bob are reading book N at the same time, i.e. the
+// amount of time necessary to read all the books combined
+// is less than twice the amount of time necessary to read book N.
+
 #include <algorithm>
 #include <iostream>
 using namespace std;
 
 int main(){
-	int n;cin>>n;
-	int books[n];
-	for(int i=0;i<n;++i)
-		cin>>books[i];
-	sort(books,books+n);
-	int r[n+1];r[0]=0;
-	for(int i=0;i<n;++i)
-		r[i+1]=r[i]+books[i];
-	int target=r[n];
-	if(target&1)target/=2;
-	else target=target/2-1;
-	int i=upper_bound(r,r+n+1,target)-r;
-	cout<<r[i]<<'\n';
+	int n; cin >> n;
+	long long books[n];
+	long long sum = 0;
+	for(int i=0; i<n; ++i)
+		cin>>books[i], sum += books[i];
+	sort(books, books+n);
+	cout << max(sum, books[n-1]*2) << '\n';
 }
