9371fdf492aa137ac4695b95d6f3cbd0dae5175b
Vincent Loh <vincentmloh87@gmail.com>
2024-05-13 21:27:24 -0400

    minor change to oset

--- review/ordered_set.h
+++ review/ordered_set.h
@@ -1,3 +1,3 @@
 #include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-#define oset __gnu_pbds::tree<int, __gnu_pbds::null_type, std::less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>
+using namespace __gnu_pbds;
+#define oset tree<int, null_type, std::less<int>, rb_tree_tag, tree_order_statistics_node_update>
