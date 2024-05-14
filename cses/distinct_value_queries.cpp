4dc5ca3cda699809d4595e805f20a82f201c50f8
Vincent Loh <vincentmloh87@gmail.com>
2024-05-14 19:28:56 -0400

    mle lmao

--- cses/distinct_value_queries.cpp
+++ cses/distinct_value_queries.cpp
@@ -1,33 +1,28 @@
-#include <set>
 #include <iostream>
+#include <bitset>
+#include <map>
 
 int main(){
 	std::cin.tie(0) -> sync_with_stdio(0);
 	int n, q; std::cin >> n >> q;
-	std::set<int> tree[n<<1];
+	std::bitset<200000> *tree = new std::bitset<200000>[n<<1];
+	std::map<int, int> val;
+	int c = 0;
 	for(int i=0; i<n; ++i){
 		int t; std::cin >> t;
-		int v = i+n;
-		while(v && !tree[v].count(t)){
-			tree[v].insert(t);
-			v >>= 1;
-		}
+		if(!val.count(t)) val[t] = c++;
+		tree[i+n][val[t]] = 1;
 	}
+	for(int i=n-1; i; --i) tree[i] = tree[i<<1] | tree[i<<1|1];
 	while(q--){
 		int a, b; std::cin >> a >> b;
-		std::set<int> ret;
+		std::bitset<200000> ret;
 		a += n-1, b += n;
 		while(a < b){
-			if(a&1){
-				for(int i : tree[a]) ret.insert(i);
-				++a;
-			}
-			if(b&1){
-				--b;
-				for(int i : tree[b]) ret.insert(i);
-			}
+			if(a&1) ret |= tree[a++];
+			if(b&1) ret |= tree[--b];
 			a >>= 1, b >>= 1;
 		}
-		std::cout << ret.size() << '\n';
+		std::cout << ret.count() << '\n';
 	}
 }
