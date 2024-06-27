f73c471dc1755f4482b6a4e0a8439ea87e956648
Vincent Loh <vincentmloh87@gmail.com>
2024-06-27 09:30:19 -0400

    finished bridgefind alg

--- review/find_bridges.cpp
+++ review/find_bridges.cpp
@@ -13,8 +13,8 @@ int main(){
 		adj[a].push_back(b), adj[b].push_back(a);
 	}
 
-	int at[n] = {};
-	bool vis[n] = {}, bridge[n] = {};
+	int at[n] = {}, depth[n] = {1};
+	bool bridge[n] = {};
 
 	int parent[n] = {-1}, delta[n] = {}, now = 0;
 
@@ -22,32 +22,38 @@ int main(){
 
 	while(dfs.size()){
 		int top = dfs.top();
-		vis[top] = 1;
 
 		if(at[top] == adj[top].size()){
 			dfs.pop(), par.pop();
 
 			now += delta[top];
 
-			if(now == 0) bridge[top] = 1;
+			if(dfs.size()){
+				if(now == 0) bridge[top] = 1;
+
+				delta[dfs.top()] += now, now = 0;
+			}
 		}else{
 			int next = adj[top][at[top]];
-			if(!vis[next]){
+			if(!depth[next]){
 				dfs.push(next), par.push(top);
 
-				parent[next] = top;
-			}else if(par.size() && par.top() != next){
-				// process back edge
+				parent[next] = top, depth[next] = depth[top] + 1;
+			}else if(par.size() && par.top() != next && depth[next] < depth[top]){
 				++delta[top];
 				--delta[next];
+
+				std::cout << "Back edge: " << top+1 << ' ' << next+1 << '\n';
 			}
 
 			++at[top];
 		}
 	}
 
+	std::cout << "Parents:\n";
 	for(int i : parent) std::cout << i+1 << ' '; std::cout << '\n';
 
+	std::cout << "Bridges:\n";
 	for(int i=0; i<n; ++i) if(bridge[i])
 		std::cout << i+1 << ' ' << parent[i]+1 << '\n';
 }
