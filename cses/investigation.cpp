0f11dbad4e0beb051e9fba1deea6b2f2dc6f8f67
Vincent Loh <vincentmloh87@gmail.com>
2024-06-28 14:07:00 -0400

    dijkstra* and it turns out I had the wrong ordering

--- cses/investigation.cpp
+++ cses/investigation.cpp
@@ -1,3 +1,6 @@
+// Investigation  -  https://cses.fi/problemset/task/1202/
+//
+// This problem is basically dijkstra spam lmao
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -15,9 +18,9 @@ int main(){
 		adj[a-1].push_back({b-1, c});
 	}
 
-	std::priority_queue<std::array<long long, 2>> pq; pq.push({0, 0});
-	int mult[n], min[n], max[n];
-	long long dist[n] = {};
+	std::priority_queue<std::array<long long, 3>> pq; pq.push({0, 0, -1});
+	long long dist[n] = {}, mult[n];
+	int min[n], max[n];
 	bool vis[n] = {};
 
 	constexpr int M = 1e9 + 7;
@@ -26,25 +29,25 @@ int main(){
 	min[0] = 0, max[0] = 0;
 
 	while(pq.size()){
-		std::array<long long, 2> top = pq.top(); pq.pop();
+		std::array<long long, 3> top = pq.top(); pq.pop();
 
-		for(const auto &[next, cost] : adj[top[1]]) if(!vis[next]){
-			pq.push({top[0] - cost, next});
+		if(!dist[top[1]]){
+			dist[top[1]] = top[0];
+			if(top[2]+1){
+				mult[top[1]] = mult[top[2]];
+				min[top[1]] = std::min(min[top[1]], min[top[2]] + 1);
+				max[top[1]] = std::max(max[top[1]], max[top[2]] + 1);
+			}
 
-			dist[next] = cost - top[0];
-			mult[next] = mult[top[1]];
-
-			min[next] = std::min(min[next], min[top[1]] + 1);
-			max[next] = std::max(max[next], max[top[1]] + 1);
-
-			vis[next] = 1;
-		}else if(cost - top[0] == dist[next]){
-			mult[next] = (mult[next] + mult[top[1]]) % M;
-
-			min[next] = std::min(min[next], min[top[1]] + 1);
-			max[next] = std::max(max[next], max[top[1]] + 1);
+			for(const auto &[next, cost] : adj[top[1]]) if(!dist[next]){
+				pq.push({top[0] - cost, next, top[1]});
+			}
+		}else if(dist[top[1]] == top[0] && top[2]+1){
+			mult[top[1]] = (mult[top[1]] + mult[top[2]]) % M;
+			min[top[1]] = std::min(min[top[1]], min[top[2]] + 1);
+			max[top[1]] = std::max(max[top[1]], max[top[2]] + 1);
 		}
 	}
 
-	std::cout << dist[n-1] << ' ' << mult[n-1] << ' ' << min[n-1] << ' ' << max[n-1] << '\n';
+	std::cout << -dist[n-1] << ' ' << mult[n-1] << ' ' << min[n-1] << ' ' << max[n-1] << '\n';
 }
