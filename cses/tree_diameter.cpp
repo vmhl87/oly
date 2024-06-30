99dfca84a1a6388086f2eb5bc3cd7c8af197e00c
Vincent Loh <vincentmloh87@gmail.com>
2024-06-30 01:14:41 -0400

    finished on mobile ide

--- cses/tree_diameter.cpp
+++ cses/tree_diameter.cpp
@@ -13,7 +13,10 @@ int main(){
 	}
 
 	std::stack<int> dfs; dfs.push(0);
-	bool vis[n] = {};
+	bool vis[n];
+	
+	int cand[n][2], best = 0;
+	for(int i=0; i<n; ++i) cand[i][0] = 0, cand[i][1] = 0, vis[i] = 0; vis[0] = 1;
 
 	while(dfs.size()){
 		int t = dfs.top();
@@ -23,6 +26,25 @@ int main(){
 
 			if(!vis[next]) dfs.push(next), vis[next] = 1;
 
-		}else dfs.pop();
+		}else{
+			best = std::max(best, cand[t][0]+cand[t][1]);
+
+			dfs.pop();
+			
+			if(dfs.size()){
+				int cur = dfs.top();
+				
+				if(cand[t][1]+1 > cand[cur][0]){
+					cand[cur][0] = cand[t][1]+1;
+					
+					if(cand[cur][0] > cand[cur][1]){
+						t = cand[cur][0];
+						cand[cur][0] = cand[cur][1], cand[cur][1] = t;
+					}
+				}
+			}
+		}
 	}
+	
+	std::cout << best << '\n';
 }
