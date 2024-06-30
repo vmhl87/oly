651c2d9150fbc1cb812264afa6ea1ffb15482637
Vincent Loh <vincentmloh87@gmail.com>
2024-06-30 18:29:56 -0400

    edi

--- cses/distance_queries.cpp
+++ cses/distance_queries.cpp
@@ -1,3 +1,6 @@
+// Distance Queries  -  https://cses.fi/problemset/task/1135/
+// Literally just binlift bash
+
 #include <iostream>
 #include <stack>
 
@@ -14,9 +17,11 @@ int main(){
 		adj[a].push(b), adj[b].push(a);
 	}
 
+	// jump, parent, depth
 	int j[n], p[n], depth[n] = {};
 	std::stack<int> dfs;
 
+	// repurpose depth arr as visited
 	dfs.push(0), j[0] = 0, depth[0] = 1;
 
 	while(dfs.size()){
@@ -28,6 +33,7 @@ int main(){
 			if(!depth[next]){
 				depth[next] = depth[t] + 1;
 
+				// skew binary tree for jump patterns [pretty standard]
 				if(depth[t] + depth[j[j[t]]] == depth[j[t]]*2)
 					j[next] = j[j[t]];
 				else j[next] = t;
@@ -46,11 +52,13 @@ int main(){
 
 		int res = depth[a] - depth[b], old = depth[b];
 
+		// balance depths
 		while(depth[a] > depth[b]){
 			if(depth[j[a]] < depth[b]) a = p[a];
 			else a = j[a];
 		}
 
+		// search for lca
 		while(a != b){
 			if(j[a] == j[b]) a = p[a], b = p[b];
 			else a = j[a], b = j[b];
