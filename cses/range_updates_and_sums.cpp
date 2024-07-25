6a5d9ee904b5c00b5e0f6401dd6f4b2901a964e2
Vincent Loh <vincentmloh87@gmail.com>
2024-07-25 02:21:17 -0400

    sqrt go bonk

--- cses/range_updates_and_sums.cpp
+++ cses/range_updates_and_sums.cpp
@@ -1,68 +1,104 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+// Range Updates And Sums  -  https://cses.fi/problemset/task/1735
 
-typedef struct segtree{
-	struct op{
-		int a=0,m=0;
-	};
-	vector<int> tree;
-	vector<int> lazy;
-	int n;
-	void prop(int i){
-		tree[i]=tree[i<<1]+tree[i<<1|1];
+// Intended to be solved by lazy segtree. I'm lazily propagating the
+// learning of such technique to a later date, so sqrt bucketing it is.
+
+#include <iostream>
+
+using LL = long long;
+// 256 is close enough to sqrt(2e5) and is a nice power of 2
+const LL sq = 256;
+
+int n;
+// array values, bucket sums, lazy {set, add} q
+LL a[200260], bucket[800], set[800], add[800];
+
+// very similar to how push() works in segtree, but no recursive,
+// as there is only one level of tree :D
+void push(int i){
+	if(set[i]){
+		for(int j=0; j<sq; ++j) a[(i*sq)+j] = set[i];
+		set[i] = 0;
 	}
-	segtree(int l):tree(l*2),lazy(l*2),n(l){
-		for(int i=0;i<l;++i)cin>>tree[i+l];
-		for(int i=l-1;i>0;--i)prop(i);
+	if(add[i]){
+		for(int j=0; j<sq; ++j) a[(i*sq)+j] += add[i];
+		add[i] = 0;
 	}
-	void comb(int i,int j){
-		lazy[i].a*=lazy[j].m;
-		lazy[i].m*=lazy[j].m;
-		lazy[i].a+=lazy[j].a;
-	}
-	void push(int i){
-		if(i<n){
-			comb(i<<1,i);
-			comb(i<<1|1,i);
-		}else{
-			tree[i]*=lazy[i].m;
-			tree[i]+=lazy[i].a;
-		}
-		lazy[i].a=0;lazy[i].m=1;
-	}
-	void set(int l,int r,int v){
-		l+=n;r+=n-1;
-		while(l<=r){
-			if(l&1){
-				lazy[l].a=v;
-				lazy[l].m=0;
-				l++;
-			}
-			if(~r&1){
-				lazy[r].a=v;
-				lazy[r].m=0;
-				r--;
-			}
-			l>>=1;r>>=1;
-		}
-	}
-}segtree;
+}
 
 int main(){
-	int n,q;cin>>n>>q;
-	segtree m(n);
+	std::cin.tie(0) -> sync_with_stdio(0);
+
+	int q; std::cin >> n >> q;
+
+	// setup buckets. very simple!
+	for(int i=0; i<n; ++i) std::cin >> a[i], bucket[i/sq] += a[i];
+
 	while(q--){
-		int t;cin>>t;
-		if(t==3){
-			int a,b;cin>>a>>b;
-			cout<<m.sum(a-1,b)<<'\n';
-		}else if(t==2){
-			int a,b,x;cin>>a>>b>>x;
-			m.set(a-1,b,x);
+		int t; std::cin >> t;
+
+		// add query
+		if(t == 1){
+			int l, r, x; std::cin >> l >> r >> x, --l, --r;
+
+			// because we'll be messing with individual array values on the
+			// end segments, we need to push waiting updates
+			push(l/sq), push(r/sq);
+
+			// only on one bucket
+			if(l/sq == r/sq) for(int i=l; i<=r; ++i) a[i] += x, bucket[l/sq] += x;
+			// across multiple buckets
+			else{
+				// intermediate buckets - place lazy add update
+				for(int i=1+l/sq; i<r/sq; ++i) add[i] += x, bucket[i] += (LL)x*sq;
+
+				// left and right sides
+				for(int i=l; i<sq*(1+l/sq); ++i) a[i] += x, bucket[l/sq] += x;
+				for(int i=sq*(r/sq); i<=r; ++i) a[i] += x, bucket[r/sq] += x;
+			}
+
+		// set query - very similar
+		}else if(t == 2){
+			int l, r, x; std::cin >> l >> r >> x, --l, --r;
+
+			push(l/sq), push(r/sq);
+
+			if(l/sq == r/sq){
+				bucket[l/sq] = 0;
+				for(int i=l; i<=r; ++i) a[i] = x;
+				for(int i=0; i<sq; ++i) bucket[l/sq] += a[i+sq*(l/sq)];
+
+			}else{
+				for(int i=1+l/sq; i<r/sq; ++i) set[i] = x, add[i] = 0, bucket[i] = (LL)x*sq;
+				
+				// recalculate buckets; no easier/better solution
+				for(int i=l; i<sq*(1+l/sq); ++i) a[i] = x;
+				bucket[l/sq] = 0;
+				for(int i=0; i<sq; ++i) bucket[l/sq] += a[i+sq*(l/sq)];
+
+				for(int i=sq*(r/sq); i<=r; ++i) a[i] = x;
+				bucket[r/sq] = 0;
+				for(int i=0; i<sq; ++i) bucket[r/sq] += a[i+sq*(r/sq)];
+			}
+
+		// range query
 		}else{
-			int a,b,x;cin>>a>>b>>x;
-			m.add(a-1,b,x);
+			int l, r; std::cin >> l >> r, --l, --r;
+
+			push(l/sq), push(r/sq);
+
+			// literally the same as update query but we don't modify
+			// values and instead accumulate a running sum
+			LL sum = 0;
+
+			if(l/sq == r/sq) for(int i=l; i<=r; ++i) sum += a[i];
+			else{
+				for(int i=1+l/sq; i<r/sq; ++i) sum += bucket[i];
+				for(int i=l; i<sq*(1+l/sq); ++i) sum += a[i];
+				for(int i=sq*(r/sq); i<=r; ++i) sum += a[i];
+			}
+
+			std::cout << sum << '\n';
 		}
 	}
 }
