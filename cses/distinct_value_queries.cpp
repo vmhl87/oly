d3d97a922b8a6f2e2d81544bafa4f024f77fd47e
Vincent Loh <vincentmloh87@gmail.com>
2024-07-25 00:34:10 -0400

    finally lol

--- cses/distinct_value_queries.cpp
+++ cses/distinct_value_queries.cpp
@@ -1,28 +1,65 @@
+// Distinct Value Queries  -  https://cses.fi/problemset/task/1734
+//
+// process queries offline; notice that for each right endpoint,
+// the set of queries on all left endpoints is monotonically
+// decreasing or something bc of influence of values:
+//
+//   1 2 3 2 4
+//   --+---+     <- only left endpoints in this region include
+//                  the 2 in their distinct values
+//
+// so we can do this by range updating a segtree (no need for lazy
+// prop as we only need point queries)
+//
+// (am tired pls ignore)
+
 #include <iostream>
-#include <bitset>
+#include <vector>
+#include <array>
 #include <map>
 
+// values, answer to queries, n
+int a[200001], ans[200001], n;
+std::vector<std::array<int, 2>> query[200001];
+
+// segtree + operations
+
+int tree[400001];
+
+void add(int l, int r){  // always just an increment
+	for(l += n, r += n; l < r; l >>= 1, r >>= 1){
+		if(l&1) ++tree[l++];
+		if(r&1) ++tree[--r];
+	}
+}
+
+int get(int i){
+	int res = 0;
+	for(i += n; i; i >>= 1) res += tree[i];
+	return res;
+}
+
 int main(){
-	std::cin.tie(0) -> sync_with_stdio(0);
-	int n, q; std::cin >> n >> q;
-	std::bitset<200000> *tree = new std::bitset<200000>[n<<1];
-	std::map<int, int> val;
-	int c = 0;
+	int q; std::cin >> n >> q;
+	for(int i=0; i<n; ++i) std::cin >> a[i];
+
+	for(int i=0; i<q; ++i){
+		int a, b; std::cin >> a >> b, --a, --b;
+		query[b].push_back({a, i});
+	}
+
+	// in our example of 1 2 3 2 4, the second 2 only increments
+	// the range from the 3..2 seeing that previous elements were
+	// already incremented by the other 2: so, we store last seen
+	// position of each element and use for index
+	std::map<int, int> last;
+
 	for(int i=0; i<n; ++i){
-		int t; std::cin >> t;
-		if(!val.count(t)) val[t] = c++;
-		tree[i+n][val[t]] = 1;
+		if(!last.count(a[i])) last[a[i]] = 0;
+		add(last[a[i]], i+1), last[a[i]] = i+1;
+
+		for(const auto &[l, x] : query[i]) ans[x] = get(l);
 	}
-	for(int i=n-1; i; --i) tree[i] = tree[i<<1] | tree[i<<1|1];
-	while(q--){
-		int a, b; std::cin >> a >> b;
-		std::bitset<200000> ret;
-		a += n-1, b += n;
-		while(a < b){
-			if(a&1) ret |= tree[a++];
-			if(b&1) ret |= tree[--b];
-			a >>= 1, b >>= 1;
-		}
-		std::cout << ret.count() << '\n';
-	}
+
+	for(int i=0; i<q; ++i) std::cout << ans[i] << '\n';
 }
