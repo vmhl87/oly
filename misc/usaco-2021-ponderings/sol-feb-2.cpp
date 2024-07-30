48b23929c03e1aaa7bcf4cc05caf22b59f4a41dd
Vincent Loh <vincentmloh87@gmail.com>
2024-07-29 22:37:54 -0400

    almost all of feb contest??

---
+++ misc/usaco-2021-ponderings/sol-feb-2.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int anc[70000], gaps[70000];
+
+int main(){
+	int n, k; std::cin >> n >> k;
+	for(int i=0; i<n; ++i) std::cin >> anc[i];
+	std::sort(anc, anc+n);
+
+	for(int i=1; i<n; ++i)
+		gaps[i-1] = std::max(0, (anc[i]/12) - (anc[i-1]/12) - 1);
+
+	std::sort(gaps, gaps+n-1);
+
+	int rem = 0;
+	for(int i=0; i<k; ++i) rem += gaps[n-2-i];
+
+	std::cout << 12 * ((anc[n-1]/12) - (anc[0]/12) + 1 - rem) << '\n';
+}
