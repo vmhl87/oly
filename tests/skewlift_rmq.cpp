cade72ae315d007c828cdaeb148c65d894727195
Vincent Loh <vincentmloh87@gmail.com>
2024-12-04 22:36:41 -0500

    trying to improve

--- tests/skewlift_rmq.cpp
+++ tests/skewlift_rmq.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
-#include <array>
 
 const int N = 2e5 + 1;
 
-std::array<int, 2> j[N];
+int jl[N], j[N];
 int a[N], back[N];
 
-void build(int i){
-	if(i-1+j[j[i-1][0]][0] == 2*j[i-1][0]){
-		j[i] = {
-			j[j[i-1][0]][0],
+void upd(int i){
+	if(jl[i] != i-1) j[i] =
 			std::min(a[i],
-					std::min(j[i-1][1],
-						j[j[i-1][0]][1]))
-		},
-			back[i-1] = i,
-			back[j[i-1][0]] = i;
-	}else j[i] = {i-1, a[i]};
+					std::min(j[i-1],
+						j[jl[i-1]]));
+	else j[i] = a[i];
+}
+
+void build(int i){
+	if(i-1+jl[jl[i-1]] == 2*jl[i-1])
+		jl[i] = jl[jl[i-1]],
+			back[jl[i-1]] = i,
+			back[i-1] = i;
+	else jl[i] = i-1;
+	upd(i);
 }
 
 void rebuild(int i){
-	build(i);
-	if(back[i]) rebuild(back[i]);
+	while(i) upd(i), i = back[i];
 }
 
 int range(int l, int r){
 	int res = a[l];
-	while(l<r){
-		if(j[r][0] >= l)
-			res = std::min(res, j[r][1]),
-				r = j[r][0];
+	while(l <= r){
+		if(jl[r] >= l)
+			res = std::min(res, j[r]),
+				r = jl[r];
 		else res = std::min(res, a[r--]);
 	}
 	return res;
 }
 
 int main(){
+	std::cin.tie(0) -> sync_with_stdio(0);
+
 	int n, q; std::cin >> n >> q;
 	for(int i=0; i<n; ++i) std::cin >> a[i+1];
 
