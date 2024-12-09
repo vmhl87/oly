3425b11217a46b5456045ff4e1ff09fb20a0ec53
Vincent Loh <vincentmloh87@gmail.com>
2024-12-08 22:08:18 -0500

    custom bitsets

--- tests/not-hashmap.cpp
+++ tests/not-hashmap.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <bitset>
 
 template <int S>
 struct not_hashmap{
 	int k[1<<S] = {}, v[1<<S] = {};
-	std::bitset<1<<S> u, d;
+	uint64_t U[1<<(S-6)] = {}, D[1<<(S-6)] = {};
 	int s, c = 0;
 
-	uint64_t splitmix64(uint64_t i){
+	inline uint64_t splitmix64(uint64_t i){
 		uint64_t z = (i += (uint64_t) 0x9e3779b97f4a7c15);
 		z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
 		z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
@@ -18,17 +17,35 @@ struct not_hashmap{
 		s = (1<<S) - 1;
 	}
 
+	inline bool u(int i){
+		return 1 & (U[i>>6] >> (i&63));
+	}
+
+	inline void u(int i, bool v){
+		uint64_t mask = (uint64_t) 1 << i;
+		U[i>>6] = (U[i>>6] & ~mask) | (-v & mask);
+	}
+
+	inline bool d(int i){
+		return 1 & (D[i>>6] >> (i&63));
+	}
+
+	inline void d(int i, bool v){
+		uint64_t mask = (uint64_t) 1 << i;
+		D[i>>6] = (D[i>>6] & ~mask) | (-v & mask);
+	}
+
 	inline int find(int i){
 		int I = splitmix64(i) & s;
-		while(u[I] && k[I] != i)
+		while(u(I) & k[I] != i)
 			I = (I+1) & s;
 		return I;
 	}
 
 	void set(int i, int j){
 		int I = find(i);
-		if(!u[I] || d[I]) ++c;
-		u[I] = 1, d[I] = 0,
+		if(!u(I) || d(I)) ++c;
+		u(I, 1), d(I, 0),
 			k[I] = i, v[I] = j;
 	}
 
@@ -38,13 +55,13 @@ struct not_hashmap{
 
 	bool has(int i){
 		int I = find(i);
-		return u[I] && !d[I] && k[I] == i;
+		return u(I) && !d(I) && k[I] == i;
 	}
 
 	void erase(int i){
 		int I = find(i);
-		if(u[I] && !d[I] && k[I] == i)
-			--c, d[I] = 1;
+		if(u(I) && !d(I) && k[I] == i)
+			--c, d(I, 1);
 	}
 
 	int size(){
@@ -53,7 +70,7 @@ struct not_hashmap{
 };
 
 int main(){
-	not_hashmap<4> m;
+	not_hashmap<7> m;
 
 	m.set(100, 2);
 	m.set(95, 8);
--- usaco/3sum.cpp
+++ usaco/3sum.cpp
@@ -38,52 +38,73 @@ ll splitmix64(ll i){
 
 template <int S>
 struct not_hashmap{
-	ll k[1<<S] = {}, v[1<<S] = {};
-	//bool u[1<<S] = {}, d[1<<S];
-	std::bitset<1<<S> u, d;
+	int k[1<<S] = {}, v[1<<S] = {};
+	uint64_t U[1<<(S-6)] = {}, D[1<<(S-6)] = {};
 	int s, c = 0;
+
+	uint64_t splitmix64(uint64_t i){
+		uint64_t z = (i += (uint64_t) 0x9e3779b97f4a7c15);
+		z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+		z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+		return z ^ (z >> 31);
+	}
 
 	not_hashmap(){
 		s = (1<<S) - 1;
 	}
 
-	inline int find(ll i){
-		//int I = i & s;
+	inline bool u(int i){
+		return 1 & (U[i>>6] >> (i&63));
+	}
+
+	inline void u(int i, bool v){
+		uint64_t mask = (uint64_t) 1 << i;
+		U[i>>6] = (U[i>>6] & ~mask) | (-v & mask);
+	}
+
+	inline bool d(int i){
+		return 1 & (D[i>>6] >> (i&63));
+	}
+
+	inline void d(int i, bool v){
+		uint64_t mask = (uint64_t) 1 << i;
+		D[i>>6] = (D[i>>6] & ~mask) | (-v & mask);
+	}
+
+	inline int find(int i){
 		int I = splitmix64(i) & s;
-		while(u[I] & k[I] != i)
+		while(u(I) & k[I] != i)
 			I = (I+1) & s;
 		return I;
 	}
 
-	void set(ll i, ll j){
+	void set(int i, int j){
 		int I = find(i);
-		if(!u[I] || d[I]) ++c;
-		u[I] = 1, d[I] = 0,
+		if(!u(I) || d(I)) ++c;
+		u(I, 1), d(I, 0),
 			k[I] = i, v[I] = j;
-	}
-
-	int get(ll i){
-		int I = find(i);
-		if(u[I] && !d[I] && k[I] == i)
-			return v[I];
-		else return 0;
 	}
 
 	bool has(int i){
 		int I = find(i);
-		return u[I] && !d[I] && k[I] == i;
+		return u(I) && !d(I) && k[I] == i;
+	}
+
+	int get(int i){
+		return has(i) ? v[find(i)] : 0;
 	}
 
 	void erase(int i){
 		int I = find(i);
-		if(u[I] && !d[I] && k[I] == i)
-			--c, d[I] = 1;
+		if(u(I) && !d(I) && k[I] == i)
+			--c, d(I, 1);
 	}
 
 	int size(){
 		return c;
 	}
 };
+
 
 void solve() {
     int n, q;
