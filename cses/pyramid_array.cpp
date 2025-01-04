941fdb173d48bd31cb4747c687592f3bed962ed6
Vincent Loh <vincentmloh87@gmail.com>
2025-01-04 09:20:04 -0500

    smth

--- cses/pyramid_array.cpp
+++ cses/pyramid_array.cpp
@@ -1,19 +1,3 @@
-/*
-7 10 4 8 5 3 1 6 2 9
-7 4 8 10 5 3 1 6 2 9 (2)
-4 7 8 10 5 3 1 6 2 9 (1)
-         9 5 3 1 6 2 (5)
-		 9 6 5 3 1 2 (3)
-		 9 6 5 3 2 1 (1)
-
-     4 8 5 3 1 6 2 9
-	 9 4 8 5 3 1 6 2 (7)
-	 9 8 4 5 3 1 6 2 (1)
-	 9 8 6 4 5 3 1 2 (4)
-	 9 8 6 5 4 3 1 2 (1)
-	 9 8 6 5 4 3 2 1 (1) -> 14
-*/
-
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 template<typename T>
@@ -21,31 +5,43 @@ using oset = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics
 
 #include <iostream>
 
+const int N = 2e5;
+
+int a[N], l[N+1], r[N+1];
+
 int main(){
 	int n; std::cin >> n;
-	int max = 0, v[n];
-	for(int i=0; i<n; ++i)
-		std::cin >> v[i], max = (v[i] > max ? v[i] : max);
-	int mi;
-	for(int i=0; i<n; ++i) if(v[i] == max) mi = i;
-	long long ret = 0;
-	oset<int> ls, rs;
-	int dist[n];
-	for(int i=mi-1; i>=0; --i){
-		auto x = ls.lower_bound(v[i]);
-		if(x == ls.end()) dist[i] = ls.size();
-		else dist[i] = ls.order_of_key(*x);
-		ls.insert(v[i]);
-	}
-	for(int i=mi+1; i<n; ++i){
-		auto x = rs.lower_bound(v[i]);
-		if(x == rs.end()) dist[i] = rs.size();
-		else dist[i] = rs.order_of_key(*x);
-		rs.insert(v[i]);
-	}
-	for(int i=0; i<mi; ++i)
-		ret += i + dist[i] - ls.order_of_key(v[i]);
-	for(int i=n-1; i>mi; --i)
-		ret += n-1 - rs.order_of_key(v[i]) - i + dist[i];
-	std::cout << ret << '\n';
+	for(int i=0; i<n; ++i) std::cin >> a[i];
+
+	{
+		oset<int> s;
+
+		for(int i=0; i<n; ++i){
+			auto x = s.upper_bound(a[i]);
+			if(x == s.end()) l[i+1] = l[i];
+			else l[i+1] = l[i]+s.size()-s.order_of_key(*x);
+			s.insert(a[i]);
+		}
+	};
+
+	{
+		oset<int> s;
+
+		for(int i=0; i<n; ++i){
+			auto x = s.upper_bound(a[n-i-1]);
+			if(x == s.end()) r[i+1] = r[i];
+			else r[i+1] = r[i]+s.size()-s.order_of_key(*x);
+			s.insert(a[n-i-1]);
+		}
+	};
+
+	// for(int i=0; i<=n; ++i) std::cout << l[i] << " \n"[i==n];
+	// for(int i=0; i<=n; ++i) std::cout << r[i] << " \n"[i==n];
+
+	int res = 1e9;
+
+	for(int i=0; i<=n; ++i)
+		res = std::min(res, l[i]+r[n-i]);
+
+	std::cout << res << '\n';
 }
